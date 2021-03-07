98a30627cbb5bc072758537d3a4c7b3afe7b9e61
Alyssa Pearson <acpearson8147@gmail.com>
2021-03-06 23:29:56 -0500

    Landscape/Item/ItemUI/Inventory
    - newly designed level
    - oliver's item, item UI, inventory scripts included

---
+++ stellae/Assets/Custom Scripts/Item.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public bool[] properties = new bool[3]; //look, inspect, pickup
+}
